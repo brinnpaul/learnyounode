ca0428cb75fe7f5e3141fd397cf4fe02df80ef24
Scott D'Alessandro <sdaless22@gmail.com>
2016-04-11 16:53:29 -0400

    Update httpClient.js

--- httpClient.js
+++ httpClient.js
@@ -4,6 +4,10 @@ url = process.argv[2];
 
 http.get(url, function(res) {
   res.setEncoding('utf8');
+  // instead of creating an annonymous function as the callback.
+  // you can just make the callback console.log, since you aren't doing anything unique inside of the function
+  // besides calling console.log
+  // res.on('data', console.log)  // the argument is passed to console.log
   res.on('data', function(d) { console.log(d); });
 }).on('error', function(er) {
   console.log(er.message);
